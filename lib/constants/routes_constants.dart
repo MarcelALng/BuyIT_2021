dc1c424cce734f5e1cadd870f95b295a6f44cb44
Marcel AL NG <43136183+MarcelALng@users.noreply.github.com>
2021-05-21 01:04:13 +0200

    [Constants/Models] create constant and screens OK

---
+++ lib/constants/routes_constants.dart
@@ -0,0 +1,10 @@
+class RoutesConstants {
+  static const String login = "/login";
+  static const String register = "/register";
+  static const String account = "/account";
+  static const String shop = "/shop";
+  static const String checkout = "/checkout";
+  static const String cart = "/cart";
+  static const String product = "/product";
+  static const String search = "/search";
+}
---
+++ lib/screens/checkout_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CheckoutScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Form(
+          child: Column(),
+        ),
+      ),
+    );
+  }
+}
