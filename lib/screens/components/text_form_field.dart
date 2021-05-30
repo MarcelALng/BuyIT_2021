89f5481897ca9efc22cea9fd01c3d8aeeea2df36
Marcel AL NG <43136183+MarcelALng@users.noreply.github.com>
2021-05-30 12:38:43 +0200

    [Firebase] New user & register, need to fix button

--- lib/screens/components/text_form_field.dart
+++ lib/screens/components/text_form_field.dart
@@ -2,24 +2,27 @@ import 'package:flutter/material.dart';
 
 class ComponentTextFormField extends StatelessWidget {
   final String hintText;
-  // final Function(String) validator;
-  // final Function(String) onSaved;
+  final Function(String) validator;
+  final Function(String) onSaved;
+  final bool secret;
 
   ComponentTextFormField({
     @required this.hintText,
-    // @required this.validator,
-    // @required this.onSaved,
+    @required this.validator,
+    @required this.onSaved,
+    this.secret = false,
   });
   @override
   Widget build(BuildContext context) {
     return Flexible(
       child: TextFormField(
-        // validator: validator,
-        // onSaved: onSaved,
+        obscureText: secret,
+        validator: validator,
+        onSaved: onSaved,
         decoration: InputDecoration(
           hintText: hintText,
           focusedBorder: UnderlineInputBorder(
-              borderSide: BorderSide(color: Colors.orangeAccent)),
+              borderSide: BorderSide(color: Colors.orangeAccent, width: 2.0)),
         ),
       ),
     );
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,21 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+import 'package:rounded_loading_button/rounded_loading_button.dart';
+import 'package:string_validator/string_validator.dart' as validate;
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'components/button_form.dart';
 import 'components/text_form_field.dart';
 
 class LoginScreen extends StatelessWidget {
+  final RoundedLoadingButtonController _buttonController =
+      RoundedLoadingButtonController();
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+    final FirebaseAuth _controllerFirebase = FirebaseAuth.instance;
+
+    final double _fontSizeTitle = MediaQuery.of(context).size.height * 0.15;
+    String _email;
+    String _password;
+
     return Scaffold(
+      // backgroundColor: Colors.black,
       body: SafeArea(
         child: Form(
+          key: _formKey,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              ComponentTextFormField(
-                hintText: "Adresse Email",
-                // validator: (){},
-                // onSaved: (){},
+              Text(
+                "BuyIT",
+                style: GoogleFonts.satisfy(
+                  color: Colors.redAccent,
+                  fontSize: _fontSizeTitle,
+                ),
               ),
+              SizedBox(height: 15.0),
+              Flexible(
+                child: Text(
+                  "Tout le High-Tech au meilleur prix",
+                  style: GoogleFonts.comfortaa(),
+                ),
+              ),
+              SizedBox(height: 5.0),
+              ComponentTextFormField(
+                hintText: "Bienvenue: Adresse Email",
+                validator: (value) {
+                  if (!validate.isEmail(value))
+                    return "Erreur adresse email incorrect";
+                  return null;
+                },
+                onSaved: (value) => _email = value,
+              ),
+              SizedBox(height: 10.0),
+              ComponentTextFormField(
+                  secret: true,
+                  hintText: "Mot de passe",
+                  validator: (value) {
+                    if (validate.isAlphanumeric(value)) {
+                      if (value.length < 6) {
+                        return "Erreur, le mot de passe doit contenir 6 caractères au minimum";
+                      }
+                      return "Erreur le mot de passe doit contenir un symbole, des lettres et des chiffres";
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _password = value;
+                  }),
+              SizedBox(height: 20.0),
+              ComponentButtonForm(
+                  onPressed: () {
+                    if (_formKey.currentState.validate()) {
+                      _formKey.currentState.save();
+                      _controllerFirebase
+                          .createUserWithEmailAndPassword(
+                              email: _email, password: _password)
+                          .then((value) => _controllerFirebase
+                              .currentUser()
+                              .then((value) => print(value.email)))
+                          .catchError((onError) => print(onError));
+                      _buttonController.success();
+                    }
+                  },
+                  controller: _buttonController),
             ],
           ),
         ),
